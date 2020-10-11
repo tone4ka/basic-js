b85c83c75d73d8f687f313fa8c290d11471c967d
tone4ka <toniamik@gmail.com>
2020-10-11 20:59:14 +0300

    docs: I'm tired:$

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+ if(!Array.isArray(arr)) throw "Error";
+ let result=arr.slice();
+ let i=0;
+while(i<result.length){
+  if(result[i]===`--discard-next`){
+    i<result.length-1?result.splice(i,2):result.splice(i,1);
+    if(result[i]===`--discard-prev`||result[i]===`--double-prev`)result.splice(i,1);
+  }
+  if(result[i]===`--discard-prev`){
+    i>0?result.splice(i-1,2):result.splice(i,1);
+  }
+   if(result[i]===`--double-next`){
+    i<result.length-1?result.splice(i,1,result[i+1]):result.splice(i,1);
+  }
+  if(result[i]===`--double-prev`){
+    i>0?result.splice(i,1,result[i-1]):result.splice(i,1);
+  }
+ else i++;
+  
+  
+}
+return result;
+
 };
--- src/what-season.js
+++ src/what-season.js
@@ -17,4 +17,3 @@ module.exports = function getSeason(date) {
         return 'autumn';
     }
 };
-// =['spring','summer','autumn','winter']
