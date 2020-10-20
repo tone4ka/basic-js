39f6813c068b9707cd263e90a18f990b097415b7
Антонина Леонидовна Зинкевич <Zinkevich@MBSP.ru>
2020-10-20 12:13:37 +0300

    feat: task complete

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let { repeatTimes, separator = '+', addition = '', additionRepeatTimes, additionSeparator = '|' } = options;
+  let additionString= new Array(additionRepeatTimes).fill(String(addition)).join(additionSeparator);
+  let result= new Array(repeatTimes).fill(String(str)+additionString).join(separator);
+  return result;
 };
   
+
+// Эта функция возвращает повторяющуюся **строку**, основываясь на заданных параметрах:
+// *	`str` это **строка**, которая будет повторена
+
+// *	`options` это **объект** опций, который содержит следующие свойства:
+//   -	`repeatTimes` устанавливает число повторений `str`
+//   - `separator` это строка, разделяющая повторения `str`
+//   - `addition` это дополнительная строка, которая будет добавлена после каждого повторения `str`
+//   - `additionRepeatTimes` устанавливает число повторений `addition`
+//   - `additionSeparator` это строка, разделяющая повторения `addition`
+
+// Параметры `str` и `addition` по умолчанию являются **строками**. В случае, если они другого типа, он должны быть преобразованы к строке.
+ 
+// Параметры `separator` и `additionSeparator` являются строками.
+
+// `repeatTimes` и `additionRepeatTimes` являются целыми числами (в случае отсутствия любого из них соответствующая строка не повторяется).
+
+// Единственный обязательный параметр — это `str`, остальные могут не быть переданы.
+// Значение `separator` по умолчанию это `'+'`. Значение `additionSeparator` по умолчанию это `'|'`.
+
+// Например: `repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3,
+//  additionSeparator: '00' })` => `'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'`
